7e063ed1fdd43493d6072821f151ae156b17a1a2
Jean Jia and Steven Colburn <jean@>
2018-06-04 11:26:30 -0700

    added foreach loop to go through flavors and display a list in html

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,19 @@
+// var favoritesOne;
+// var favoritesTwo;
 
+$(function(){
+
+  var favoriteFlavors = ["Mint", "Cookie-Dough", "Coconut", "Sweet Cream", "Matcha",  "Vanilla", "Chocolate"];
+
+  favoriteFlavors.forEach(function(flavor){
+    $("#flavor-list").append(wrapListTag(flavor));
+  });
+
+
+});
+
+
+function wrapListTag(value)
+{
+  return ("<li>"+value+"</li>");
+}
